1808fa782e0c753acc573480b4c1d5077bdfa996
Ville Salonen <ville.salonen@iki.fi>
2013-04-02 18:42:45 +0300

    Added a page for untagged files.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -214,3 +214,14 @@ function SingleTagCtrl($scope, $routeParams, LaputinAPI) {
     };
 }
 SingleTagCtrl.$inject = ['$scope', '$routeParams', 'LaputinAPI'];
+
+function UntaggedFilesCtrl($scope, LaputinAPI) {
+    $scope.untaggedFiles = [];
+
+    LaputinAPI.getFiles(function (data) {
+        $scope.untaggedFiles = _.filter(data, function (file) {
+            return file.tags.length === 0;
+        });
+    });
+}
+UntaggedFilesCtrl.$inject = ['$scope', 'LaputinAPI'];
