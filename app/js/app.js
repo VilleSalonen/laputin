5c2511b6ad16a5e0c25deab04d246093cfc1e539
Ville Salonen <ville.salonen@iki.fi>
2013-05-04 19:07:24 +0300

    Removed single file page route

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,6 @@ require("./directives.js");
 angular.module('laputin', ['laputin.filters', 'laputin.services', 'laputin.directives', 'laputin.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/files/', {templateUrl: 'partials/files.html', controller: "FilesCtrl"});
-    $routeProvider.when('/files/:fileId', {templateUrl: 'partials/single_file.html', controller: "SingleFileCtrl"});
     $routeProvider.when('/tags/', {templateUrl: 'partials/tags.html', controller: "TagsCtrl"});
     $routeProvider.when('/tags/:tagId', {templateUrl: 'partials/single_tag.html', controller: "SingleTagCtrl"});
     $routeProvider.otherwise({redirectTo: '/files/'});
