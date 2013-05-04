13eebfebaefbd9ad1174e8e48c441eb0c3017915
Ville Salonen <ville.salonen@iki.fi>
2013-05-04 20:34:04 +0300

    Removed duplicate code from SingleFileCtrl

--- app/js/controllers/SingleFileCtrl.js
+++ app/js/controllers/SingleFileCtrl.js
@@ -11,56 +11,6 @@ function SingleFileCtrl($scope, LaputinAPI) {
     $scope.closeEditing = function () {
         $scope.editing = false;
     };
-
-    $scope.open = function () {
-        LaputinAPI.openFile($scope.file);
-    };
-
-    $scope.addTag = function (tag) {
-        $scope.file.tags.push(tag);
-        LaputinAPI.linkTagToFile(tag, $scope.file, function (err) {
-            if (err) {
-                var idx = $scope.file.tags.indexOf(tag);
-                if (idx !== -1) {
-                    $scope.file.tags.splice(idx, 1);
-                }
-                alert(err);
-            }
-        });
-        $scope.availableTagQuery = "";
-    };
-
-    $scope.removeTag = function (tag) {
-        var idx = $scope.file.tags.indexOf(tag);
-        if (idx !== -1) {
-            $scope.file.tags.splice(idx, 1);
-            LaputinAPI.unlinkTagFromFile(tag, $scope.file);
-        }
-    };
-
-    $scope.createNewTag = function () {
-        LaputinAPI.createNewTag($scope.newTagName, function (tag) {
-            $scope.newTagName = "";
-            $scope.addTag(tag);
-        });
-    };
-
-    $scope.addTagFromSearch = function () {
-        var tag = _.find($scope.tags, function (tag) {
-            return tag.name === $scope.tagName;
-        });
-
-        if (tag) {
-            $scope.addTag(tag);
-            $scope.tagName = "";
-        }
-    };
-
-    $scope.tagSearchFn = function() {
-        return $.map($scope.tags, function(candidate) {
-            return candidate.name;
-        });
-    };
 };
 
 module.exports = SingleFileCtrl;
