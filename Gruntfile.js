ca213f700f0dc0af1953ba4d2ea4d63785e0907c
Ville Salonen <ville.salonen@iki.fi>
2013-06-09 09:35:43 +0300

    Added grunt watch so that browserify can be automatically executed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,27 @@
 module.exports = function (grunt) {
-  'use strict';
+    'use strict';
 
-  grunt.initConfig({
-    browserify: {
-      default: {
-        src: "app/js/app.js",
-        dest: "app/js/bundle.js"
-      }
-    }
-  });
-  
-  grunt.loadNpmTasks("grunt-browserify");
-  
-  grunt.registerTask("default", ["browserify"]);
+    grunt.initConfig({
+        browserify: {
+            default: {
+                src: "app/js/app.js",
+                dest: "app/js/bundle.js"
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['app/js/**/*.js'],
+                tasks: ['browserify'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-browserify");
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask("default", ["browserify"]);
 };
