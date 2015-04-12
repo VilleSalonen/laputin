2fd07e5d2d2cea06fcff7c4b8941e00e40969273
Ville Salonen <ville.salonen@iki.fi>
2015-04-12 07:16:48 +0300

    Rewrote tag component because previous implementation kept losing some tags. Custom component gives far better control anyway.

--- react/js/components/TagAutocompletion.js
+++ react/js/components/TagAutocompletion.js
@@ -4,6 +4,14 @@ var React = require("react");
 var LaputinAPI = require("../utils/LaputinAPI");
 
 var TagAutocompletion = React.createClass({
+    _keyEvents: {
+        38: "UP",
+        40: "DOWN",
+        13: "RETURN",
+        14: "ENTER",
+        27: "ESC"
+    },
+
     getInitialState: function () {
         var self = this;
 
@@ -24,6 +32,59 @@ var TagAutocompletion = React.createClass({
         };
     },
 
+    handleChange: function (event) {
+        // This method has to use received user input to update userInput state
+        // variable. Otherwise userInput state won't get updated and hence the
+        // input element appears to be readonly.
+
+        var matching = _.filter(this.state.tags, function (tag) {
+            return tag.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1;
+        });
+        matching = _.difference(matching, this.props.selectedTags);
+
+        this.setState({ userInput: event.target.value, matchingTags: matching });
+    },
+
+    _onKeyDown: function (event) {
+        var key = this._keyEvents[event.keyCode];
+        if (typeof key !== "undefined") {
+            switch (key) {
+                case "UP":
+                    event.preventDefault();
+                    if (this.state.selectedIndex === -1) {
+                        // can't go higher
+                        return;
+                    }
+
+                    this.setState({ selectedIndex: this.state.selectedIndex - 1 });
+                    break;
+                case "DOWN":
+                    event.preventDefault();
+                    if (this.state.selectedIndex === this.state.matchingTags.length - 1) {
+                        // can't go lower
+                        return;
+                    }
+
+                    this.setState({ selectedIndex: this.state.selectedIndex + 1 });
+                    break;
+                case "ENTER":
+                case "RETURN":
+                    event.preventDefault();
+                    if (this.state.selectedIndex === -1) {
+                        // can't select nothing
+                        return;
+                    }
+
+                    this.selectTag(this.state.matchingTags[this.state.selectedIndex]);
+                    break;
+                case "ESC":
+                    event.preventDefault();
+                    event.target.blur();
+                    this.setState({ matchingTags: [], userInput: "", selectedIndex: -1 });
+            }
+        }
+    },
+
     selectTag: function (tag) {
         this.props.callback(tag);
         this.setState({ matchingTags: [], userInput: "", selectedIndex: -1 });
@@ -33,41 +94,33 @@ var TagAutocompletion = React.createClass({
         this.setState({ userInput: event.target.value });
     },
 
-    _onKeyDown: function (event) {
-        switch (event.keyCode) {
-            case 13: // Return
-            case 14: // Enter
-                event.preventDefault();
-
-                var tag = _.find(this.state.tags, function (tag) {
-                    return tag.name === event.target.value;
-                });
-
-                if (tag) {
-                    this.selectTag(tag);
-                }
-                break;
-        }
-    },
-
     render: function() {
-        var selectedIds = _.pluck(this.props.selectedTags, "id");
-        var tags = _.filter(this.state.tags, function (tag) { return selectedIds.indexOf(tag.id) === -1; });
+        var self = this;
+
+        var tagList;
+        if (this.state.matchingTags.length > 0)
+        {
+            tagList = <div className="typeahead-list-container">
+                <ul className="typeahead-list" role="menu">
+                    {this.state.matchingTags.map(function (tag, i) {
+                        var className = (self.state.selectedIndex === i) ? "hover" : "";
+                        return React.createElement("li", {
+                                className: className,
+                                onClick: self.selectTag.bind(self, tag)
+                            },
+                            tag.name);
+                    })}
+                </ul>
+            </div>;
+        }
 
         return <div>
-            <input className="form-control"
-                   list="tagsList"
-                   id="tagAutocomplete"
+            <input className="typeahead-input form-control"
+                   type="text"
                    value={this.state.userInput}
-                   onChange={this._onChange}
-                   onKeyDown={this._onKeyDown}
-                   type="text" />
-
-            <datalist id="tagsList">
-                {tags.map(function (tag) {
-                    return <option value={tag.name} />
-                })}
-            </datalist>
+                   onChange={this.handleChange}
+                   onKeyDown={this._onKeyDown} />
+            {tagList}
         </div>;
     }
 });
