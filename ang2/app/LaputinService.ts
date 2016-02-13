c101df05080473046a0d6657710ec4c81fb8b179
Ville Salonen <ville.salonen@iki.fi>
2016-02-13 14:57:49 +0200

    Moved HTTP requests to LaputinService

--- ang2/app/LaputinService.ts
+++ ang2/app/LaputinService.ts
@@ -1,7 +1,48 @@
+import {Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
+import {Http, HTTP_PROVIDERS, Headers} from 'angular2/http';
+
 import { File } from "./file";
 import { Tag } from "./tag";
 
+@Injectable()
 export class LaputinService {
-    public tags : Tag[];
-    public files : File[];
+    public tags : Observable<Tag[]>;
+    private _tagObserver: any;
+    
+    public files : Observable<File[]>;
+    private _fileObserver: any;
+    
+    constructor(private _http: Http) {
+        this.tags = new Observable<Tag[]>((observer: any) => this._tagObserver = observer).share();
+        this.files = new Observable<File[]>((observer: any) => this._fileObserver = observer).share();
+    }
+    
+    public loadTags() {
+        this._http.get('http://localhost:12345/tags')
+            .map(res => res.json())
+            .subscribe(data => {
+                var converted = Observable.fromArray(data).map((tag: any) => new Tag(tag.id, tag.name, tag.associationCount));
+                this._tagObserver.next(converted);
+            });
+            
+        this._http.get('http://localhost:12345/files')
+            .map(res => res.json())
+            .subscribe(data => {
+                var converted = Observable.fromArray(data).map((file: any) => new File(file.hash, file.path, []));
+                this._fileObserver.next(converted);
+            });
+    }
+    
+    public openFile(file: File) {
+        let body = JSON.stringify({ selectedHashes: [file.hash] });
+        const headers = new Headers({'Content-Type': 'application/json'});
+        
+        this._http.post("http://localhost:12345/open/files/", body, { headers: headers })
+                   .subscribe(
+                        data => console.log("Data: " + data),
+                        err => console.log("Error: " + err),
+                        () => console.log("Complete")
+                   );
+    }
 }
--- ang2/app/app.component.ts
+++ ang2/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from 'angular2/core';
-import {Http, HTTP_PROVIDERS, Headers} from 'angular2/http';
+import {Component, OnInit} from 'angular2/core';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {Observable} from 'rxjs/Rx';
 
 import { File } from "./file";
 import { Tag } from "./tag";
@@ -30,39 +31,28 @@ import { LaputinService } from "./laputinservice";
             </ul>
         </h2>
     `,
-    providers: [HTTP_PROVIDERS]
+    providers: [LaputinService, HTTP_PROVIDERS]
 })
-export class AppComponent {
-    service: LaputinService;
-    http: Http;
-    
+export class AppComponent implements OnInit {
     public files : File[];
     public tags : Tag[];
     
-    constructor(http: Http) {
-        this.service = new LaputinService();
-        this.http = http;
-        
-        http.get('http://localhost:12345/tags').subscribe(res => {
+    constructor(private _service: LaputinService) {
+    }
+    
+    ngOnInit() {
+        this._service.tags.subscribe(data => {
             this.tags = [];
-            res.json().map((tag: any) => this.tags.push(new Tag(tag.id, tag.name, tag.associationCount)));
+            data.forEach(row => this.tags.push(row));
         });
-        
-        http.get('http://localhost:12345/files').subscribe(res => {
+        this._service.files.subscribe(data => {
             this.files = [];
-            res.json().map((file: any) => this.files.push(new File(file.hash, file.path, [])));
+            data.forEach(row => this.files.push(row));
         });
+        this._service.loadTags();
     }
     
     onSelect(file: File) {
-        let body = JSON.stringify({ selectedHashes: [file.hash] });
-        const headers = new Headers({'Content-Type': 'application/json'});
-        
-        this.http.post("http://localhost:12345/open/files/", body, { headers: headers })
-                   .subscribe(
-                        data => console.log("Data: " + data),
-                        err => console.log("Error: " + err),
-                        () => console.log("Complete")
-                   );
+        this._service.openFile(file);
     }
 }
