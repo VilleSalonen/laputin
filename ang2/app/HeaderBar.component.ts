b2f4059ea848ffa2f061b35286657057fd4bec93
Ville Salonen <ville.salonen@iki.fi>
2016-02-13 15:48:07 +0200

    Added Bootstrap, CSS from React version and made some edits to layout.

---
+++ ang2/app/HeaderBar.component.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'header-bar',
+    template: `
+        <nav className="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="/#/">Laputin</a>
+                </div>
+
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav">
+                        <li><a href="/#/">Files</a></li>
+                        <li><a href="/#/tags">Tags</a></li>
+                        <li><a href="/#/duplicates">Duplicates</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `
+})
+export class HeaderBarComponent {
+}
+
--- ang2/app/app.component.ts
+++ ang2/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from 'angular2/core';
 import {HTTP_PROVIDERS, Headers} from 'angular2/http';
 import {Observable} from 'rxjs/Rx';
 
+import {HeaderBarComponent} from "./headerbar.component";
 import { File } from "./file";
 import { Tag } from "./tag";
 import { LaputinService } from "./laputinservice";
@@ -9,30 +10,44 @@ import { LaputinService } from "./laputinservice";
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Files</h1>
-        <h2>
-            <ul>
-                <li *ngFor="#file of files">
-                    {{file.path}}
-                    
-                    <ul>
-                        <li *ngFor="#tag of file.tags">
-                            {{tag.name}}
-                        </li>
-                    </ul>
-                    
-                    <button (click)="onSelect(file)">
-                        <button>Open</button>
-                    </button>
-                </li>
-            </ul>
-        </h2>
+        <header-bar></header-bar>
+    
+        <table className="table table-striped">
+            <tbody>
+                <tr>
+                    <th>
+                        Showing {{files.length}} matching files
+
+                        <a className="btn btn-primary pull-right">
+                            Open files
+                        </a>
+                    </th>
+                </tr>
+
+                <tr *ngFor="#file of files">
+                    <td>
+                        {{file.path}}
+                        
+                        <p>
+                            <span *ngFor="#tag of file.tags">
+                                {{tag.name}}
+                            </span>
+                        </p>
+                        
+                        <button (click)="onSelect(file)">
+                            <button>Open</button>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     `,
-    providers: [LaputinService, HTTP_PROVIDERS]
+    providers: [LaputinService, HTTP_PROVIDERS],
+    directives: [HeaderBarComponent]
 })
 export class AppComponent implements OnInit {
-    public files : File[];
-    public tags : Tag[];
+    public files : File[] = [];
+    public tags : Tag[] = [];
     
     constructor(private _service: LaputinService) {
     }
