55cad939f7f0debe5fe1ca9af1721be95d7f60a3
Ville Salonen <ville.salonen@iki.fi>
2016-02-13 15:55:06 +0200

    Fixed header bar styling.

--- ang2/app/HeaderBar.component.ts
+++ ang2/app/HeaderBar.component.ts
@@ -3,14 +3,14 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'header-bar',
     template: `
-        <nav className="navbar navbar-inverse navbar-static-top" role="navigation">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <a className="navbar-brand" href="/#/">Laputin</a>
+        <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/#/">Laputin</a>
                 </div>
 
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
                         <li><a href="/#/">Files</a></li>
                         <li><a href="/#/tags">Tags</a></li>
                         <li><a href="/#/duplicates">Duplicates</a></li>
