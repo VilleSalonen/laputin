03811044a7a43774c6e83d79517d3f198571fa56
Ville Salonen <ville.salonen@iki.fi>
2016-02-16 18:47:47 +0200

    Added hosting of static media
    For now all media is hosted from test-archive-no-commit directory which is located as a subdirectory in server application directory. These will not be committed to git.

--- ts-server/index.ts
+++ ts-server/index.ts
@@ -1,10 +1,14 @@
 import express = require("express");
 
-var app = express();
+var libraryPath: string = "test-archive-no-commit";
+
+var app: express.Express = express();
 
 app.get('/', (req, res) => {
     res.send('Testing 1 2 3');
 });
+
+app.use("/media", express.static(libraryPath));
 
 var port: number = +process.env.PORT || 3200;
 
