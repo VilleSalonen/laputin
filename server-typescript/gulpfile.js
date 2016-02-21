ba1281522610180fe7e40e0d77afa0c2ef312758
Ville Salonen <ville.salonen@iki.fi>
2016-02-21 10:27:44 +0200

    Added gulp task for executing tests

--- server-typescript/gulpfile.js
+++ server-typescript/gulpfile.js
@@ -1,11 +1,25 @@
 var gulp = require("gulp"),
     ts = require("gulp-typescript"),
-    nodemon = require("gulp-nodemon");
+    nodemon = require("gulp-nodemon"),
+    mocha = require("gulp-mocha"),
+    util = require("gulp-util");
     
 gulp.task("build", function () {
     console.log("Compiling TypeScript");
     return gulp.src(["*.ts"])
                .pipe(ts({module: "commonjs"})).js.pipe(gulp.dest("deploy"));
+});
+
+gulp.task("build-tests", function () {
+    console.log("Compiling tests");
+    return gulp.src(["tests/*.ts"])
+               .pipe(ts({module: "commonjs"})).js.pipe(gulp.dest("deploy-tests"));
+});
+
+gulp.task("test", ["build-tests"], function () {
+    return gulp.src(['deploy-tests/*.js'], { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', util.log);
 });
 
 gulp.task("watch", function () {
