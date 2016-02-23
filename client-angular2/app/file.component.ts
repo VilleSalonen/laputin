60c121cfc127d00de6a28e261555c38a817ebdbe
Ville Salonen <ville.salonen@iki.fi>
2016-02-23 22:16:58 +0200

    Improved layout for file tagging

--- client-angular2/app/file.component.ts
+++ client-angular2/app/file.component.ts
@@ -9,27 +9,49 @@ import {TagAutocompleteComponent} from "./tagautocomplete.component";
 @Component({
     selector: "file-row",
     template: `
-        <a (click)="toggle()" [style.font-weight]="detailsOpen ? 'bold' : 'normal'">{{file.path}}</a>
+        <div>
+            <div *ngIf="!detailsOpen">
+                <p><a (click)="toggle()">{{file.path}}</a></p>
+                
+                <p>
+                    <span *ngFor="#tag of file.tags">
+                        {{tag.name}}
+                    </span>
+                </p>
+            </div>
         
-        <p>
-            <span *ngFor="#tag of file.tags">
-                {{tag.name}}
-            </span>
-        </p>
-        
-        <div *ngIf="detailsOpen">
-            <p><img src="http://localhost:3200/media/{{file.name}}" width="320" /></p>
+            <div *ngIf="detailsOpen">
+                <p><strong><a (click)="toggle()">{{file.path}}</a></strong></p>
             
-            <p>
-                <tag-autocomplete (select)="addTag($event)"></tag-autocomplete>
-            </p>
-            
-            <p>
-                <input type="text" [(ngModel)]="newTag" />
-                <button (click)="addNewTag()">
-                    <button>Add tag</button>
-                </button>
-            </p>
+                <div class="row">
+                    <div class="col-md-2">
+                        <p>
+                            <tag-autocomplete (select)="addTag($event)"></tag-autocomplete>
+                        </p>
+                        
+                        <p>
+                            <small><a (click)="toggleTagCreation()">Didn't find the tag you were looking for..?</a></small>
+                        </p>
+                        
+                        <div *ngIf="tagCreationOpen">
+                            <form>
+                                <input type="text" [(ngModel)]="newTag" placeholder="Create a new tag" class="form-control" />
+                                <button (click)="addNewTag()">Add tag</button>
+                            </form>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-10">
+                        <p><img src="http://localhost:3200/media/{{file.name}}" width="320" /></p>
+                    
+                        <p>
+                            <span *ngFor="#tag of file.tags">
+                                {{tag.name}}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>`,
     providers: [LaputinService, HTTP_PROVIDERS],
     directives: [TagAutocompleteComponent]
@@ -38,13 +60,19 @@ export class FileRowComponent {
     public newTag: string = "";
     
     @Input() file: File;
+    
     public detailsOpen: boolean = false;
+    public tagCreationOpen: boolean = false;
     
     constructor(private _service: LaputinService) {
     }
     
     public toggle(): void {
         this.detailsOpen = !this.detailsOpen;
+    }
+    
+    public toggleTagCreation(): void {
+        this.tagCreationOpen = !this.tagCreationOpen;
     }
     
     public addNewTag(): void {
