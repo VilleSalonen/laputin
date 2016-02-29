1515779cbe7dd4137d1b7a123b7d265bbc5f2f7d
Ville Salonen <ville.salonen@iki.fi>
2016-02-29 19:06:20 +0200

    Employed async-await in Laputin

--- server-typescript/server.ts
+++ server-typescript/server.ts
@@ -30,21 +30,25 @@ export class Laputin {
         this.app.use(cors());
         this.app.use(bodyParser.json({}));
 
-        this.app.route("/files").get((req, res) => {
-            this.library.getFiles(req.query)
-                .then((files) => { res.send(files); });
+        this.app.route("/files").get(async (req, res) => {
+            let files = await this.library.getFiles(req.query);
+            res.send(files);
         });
 
-        this.app.route("/tags").get((req, res) => {
-            this.library.getTags(req.query)
-                .then((tags) => { res.send(tags); });
+        this.app.route("/tags").get(async (req, res) => {
+            let tags = await this.library.getTags(req.query);
+            res.send(tags);
         });
 
-        this.app.route("/tags").post((req, res) => {
-            var tagName = req.body.tagName;
-            this.library.createNewTag(tagName)
-                .then((tag) => { res.send(tag); })
-                .catch(() => res.status(500).end());
+        this.app.route("/tags").post(async (req, res) => {
+            try {
+                let tagName = req.body.tagName;
+                let tag = await this.library.createNewTag(tagName);
+                res.send(tag);
+            }
+            catch (error) {
+                res.status(500).end();
+            }
         });
 
         this.app.route("/files/:hash/tags").post((req, res) => {
@@ -55,16 +59,16 @@ export class Laputin {
             res.status(200).end();
         });
 
-        this.app.route("/files/:hash/tags/:tagId").delete((req, res) => {
-            this.library.deleteLinkBetweenTagAndFile(req.params.tagId, req.params.hash)
-                .then(() => { res.status(200).end(); });
+        this.app.route("/files/:hash/tags/:tagId").delete(async (req, res) => {
+            await this.library.deleteLinkBetweenTagAndFile(req.params.tagId, req.params.hash);
+            res.status(200).end();
         });
 
         this.app.use("/media", express.static(this._libraryPath));
     }
     
-    public loadFiles(): Promise<void> {
-        return this.library.deactivateAll()
-            .then(() => { return this.fileLibrary.load(); });
+    public async loadFiles(): Promise<void> {
+        await this.library.deactivateAll();
+        return this.fileLibrary.load();
     }
 }
