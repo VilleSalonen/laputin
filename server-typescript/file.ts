65707bb0c1b1d1ff356c05eb8f8173245ebaae0c
Ville Salonen <ville.salonen@iki.fi>
2016-03-01 17:48:52 +0200

    Changed File.name from getter to field to allow easy serialization

--- server-typescript/file.ts
+++ server-typescript/file.ts
@@ -5,15 +5,17 @@ import {Tag} from "./tag";
 export class File {
     public hash: string;
     public path: string;
+    public name: string;
     public tags: Tag[];
     
     constructor(hash: string, path: string, tags: Tag[]) {
         this.hash = hash;
         this.path = path.replace("\\", "/");
+        this.name = this.cleanPath(); 
         this.tags = tags;
     }
-
-    public get name(): string {
+    
+    private cleanPath(): string {
         return path.basename(this.path);
-    } 
+    }
 }
