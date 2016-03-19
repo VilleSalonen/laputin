839566f48bcc3af3b0044cec5d84be8eb780e155
Ville Salonen <ville.salonen@iki.fi>
2016-03-19 17:01:16 +0200

    Ugly hack to fix crashing of MD5 hasher when files are in the middle of being copied to library path

--- src/quickmd5hasher.ts
+++ src/quickmd5hasher.ts
@@ -14,18 +14,26 @@ export class QuickMD5Hasher implements IHasher {
         var done: Function;
         var promise = new Promise<File>((resolve, reject) => done = resolve);
 
-        fs.open(path, "r", function(err, fd) {
-            fs.stat(path, function(err, stats) {
-                // Sometimes stats is undefined. This is probably due to file being
-                // moved elsewhere or deleted.
-                if (typeof stats === "undefined")
+        try {
+            fs.open(path, "r", function(err, fd) {
+                if (err) return;
+
+                if (typeof fd === "undefined")
                     return;
 
-                var input_size: number = stats.size;
-                var offset: number = input_size / 2.0 - CHUNK_SIZE / 2.0;
-                var buffer: Buffer = new Buffer(CHUNK_SIZE);
+                fs.stat(path, function(err, stats) {
+                    if (err) return;
 
-                try {
+                    // Sometimes stats is undefined. This is probably due to file being
+                    // moved elsewhere or deleted.
+                    if (typeof stats === "undefined")
+                        return;
+
+                    var input_size: number = stats.size;
+                    var offset: number = input_size / 2.0 - CHUNK_SIZE / 2.0;
+                    var buffer: Buffer = new Buffer(CHUNK_SIZE);
+
+
                     fs.read(fd, buffer, 0, buffer.length, offset, function(e, l, b) {
                         var dataForHashing = b.toString("binary");
                         var hash = crypto.createHash("md5")
@@ -36,18 +44,19 @@ export class QuickMD5Hasher implements IHasher {
 
                         done({ path: path, hash: hash });
                     });
-                }
-                catch (e) {
-                    if (e.name === "TypeError" && e.message === "Bad argument") {
-                        // If hashing is done when file is still being copied, it will
-                        // fail.
-                    }
-                    else {
-                        throw e;
-                    }
-                }
+                });
             });
-        });
+        }
+        catch (e) {
+            console.log(e);
+            if (e.name === "TypeError") {
+                // If hashing is done when file is still being copied, it will
+                // fail.
+            }
+            else {
+                throw e;
+            }
+        };
 
         return promise;
     }
