e0457a88c4d5b62e5947c8d1993d3c0b98b335ef
Ville Salonen <ville.salonen@iki.fi>
2016-11-04 12:19:21 +0200

    added top menu

--- src/client2/src/app/app.component.ts
+++ src/client2/src/app/app.component.ts
@@ -2,7 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">Laputin</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a [routerLink]="['/files']">Files</a></li>
+                    <li><a [routerLink]="['/tags']">Tags</a></li>
+                    <li><a [routerLink]="['/duplicates']">Duplicates</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
