49086220d1d0335d33416cb9749d08dbc61de87d
Ville Salonen <ville.salonen@iki.fi>
2016-11-07 21:01:41 +0200

    Code climate cleaning (#24)
    * Delete duplicates.component.css
    
    * Delete file-row.component.css
    
    * Delete file-search.component.css
    
    * Delete files.component.css
    
    * Delete search-box.component.css
    
    * Delete search-tag.component.css
    
    * Delete tag-autocomplete.component.css
    
    * Delete tags.component.css
    
    * Delete app.component.css
    
    * Removed css reference

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -20,9 +20,7 @@ import { Component } from '@angular/core';
     </nav>
 
     <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.css']
+  `
 })
 export class AppComponent {
-  title = 'app works!';
 }
