a070504e454d270cdb9a07a2eddfa02b7b20f513
Ville Salonen <ville.salonen@iki.fi>
2017-08-13 20:50:59 +0300

    Improved the look of tags view

--- src/client/src/app/tags/tags.component.ts
+++ src/client/src/app/tags/tags.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Injectable, Inject} from "@angular/core";
+import * as _ from "lodash";
 
 import {File} from "./../models/file";
 import {Tag} from "./../models/tag";
@@ -6,25 +7,61 @@ import {LaputinService} from "./../laputin.service";
 
 @Component({
     template: `
-        <search-box (update)="term = $event"></search-box>
-        <ul>
-            <li *ngFor="let tag of tags | tagfilter: term">
-                <tag-row [tag]="tag"></tag-row>
-            </li>
-        </ul>
+        <div class="filter-controls" style="margin-top: -20px; margin-bottom: 0px;">
+            <div class="extra-padded">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form class="form-horizontal">
+                            <div class="form-group col-md-4">
+                                <label class="col-sm-2 control-label">Name</label>
+                                <div class="col-sm-10">
+                                    <div>
+                                        <search-box (update)="filter($event)"></search-box>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <th>
+                        Showing {{filteredTags.length}} matching tags
+                    </th>
+                </tr>
+
+                <tr *ngFor="let tag of filteredTags">
+                    <td>
+                        <tag-row [tag]="tag"></tag-row>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     `,
     providers: [LaputinService]
 })
 @Injectable()
 export class TagsComponent implements OnInit {
     public tags: Tag[] = [];
-    public term: string = "";
+    public filteredTags: Tag[] = [];
     
     constructor(@Inject(LaputinService) private _service: LaputinService) {
     }
     
     ngOnInit(): void {
         this._service.getTags()
-            .then((tags) => this.tags = tags);
+            .then((tags) => {
+                this.tags = tags;
+                this.filteredTags = tags;                
+            });
+    }
+
+    filter(term: string) {
+        let termUpperCase = term.toUpperCase();
+        this.filteredTags = _.filter(this.tags, (tag) => tag.name.toUpperCase().indexOf(termUpperCase) >= 0);
     }
 }
