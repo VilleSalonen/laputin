d4a021368917379ca5fa4b90baf7916214cca11c
Ville Salonen <ville.salonen@iki.fi>
2017-08-13 20:59:53 +0300

    Improved the look of duplicates view

--- src/client/src/app/duplicates/duplicates.component.ts
+++ src/client/src/app/duplicates/duplicates.component.ts
@@ -1,32 +1,68 @@
 import {Component, Injectable, Inject} from "@angular/core";
+import * as _ from "lodash";
 
 import {LaputinService} from "./../laputin.service";
 import {Duplicate} from "./../models/duplicate";
 
 @Component({
     template: `
-        <h1>Duplicates</h1>
-        
-        <p [hidden]="duplicates.length > 0">No duplicates found.</p>
-        
-        <ul>
-            <li *ngFor="let duplicate of duplicates">
-                {{duplicate.hash}}
-                <ul>
-                    <li *ngFor="let file of duplicate.files">
-                        {{file.path}}
-                    </li>
-                </ul>
-            </li>
-        </ul>
+        <div class="filter-controls" style="margin-top: -20px; margin-bottom: 0px;">
+            <div class="extra-padded">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form class="form-horizontal">
+                            <div class="form-group col-md-4">
+                                <label class="col-sm-2 control-label">Name</label>
+                                <div class="col-sm-10">
+                                    <div>
+                                        <search-box (update)="filter($event)"></search-box>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <th>
+                        <span [hidden]="duplicates.length == 0">Showing {{filteredDuplicates.length}} matching duplicates</span>
+                        <span [hidden]="duplicates.length > 0">No duplicates found.</span>
+                    </th>
+                </tr>
+
+                <tr *ngFor="let duplicate of filteredDuplicates">
+                    <td>
+                        {{duplicate.hash}}
+                        <ul>
+                            <li *ngFor="let file of duplicate.files">
+                                {{file.path}}
+                            </li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     `,
     providers: [LaputinService]
 })
 @Injectable()
 export class DuplicatesComponent {
     public duplicates: Duplicate[] = [];
+    public filteredDuplicates: Duplicate[] = [];
     
     constructor(@Inject(LaputinService) private _service: LaputinService) {
-        _service.getDuplicates().then((duplicates: Duplicate[]) => this.duplicates = duplicates);
+        _service.getDuplicates().then((duplicates: Duplicate[]) => {
+            this.duplicates = duplicates;
+            this.filteredDuplicates = duplicates;
+        });
+    }
+
+    filter(term: string) {
+        let termUpperCase = term.toUpperCase();
+        this.filteredDuplicates = _.filter(this.duplicates, (duplicate) => _.some(duplicate.files, (file) => file.name.toUpperCase().indexOf(termUpperCase) >= 0));
     }
 }
