ffe9dfebb53235c9816cecd8df771e3fc16b8402
Ville Salonen <ville.salonen@iki.fi>
2017-10-02 21:34:56 +0300

    some color improvements, still ugly

--- src/client/src/app/search-box/search-box.component.ts
+++ src/client/src/app/search-box/search-box.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
     selector: 'app-search-box',
+    styleUrls: ['./search-box.component.scss'],
     template: `
     <div>
         <input
