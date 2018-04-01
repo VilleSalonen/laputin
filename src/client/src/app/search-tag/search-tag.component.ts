2310b2609132566edf2993764f410fcb032c68a2
Ville Salonen <ville.salonen@iki.fi>
2018-04-01 23:03:44 +0300

    some improvements to search-tag, positioning is still a bit off

--- src/client/src/app/search-tag/search-tag.component.ts
+++ src/client/src/app/search-tag/search-tag.component.ts
@@ -14,13 +14,13 @@ export class TagChange {
     <div class="btn-group {{tagClass}}">
         <a class="btn" (click)="removeTag()">{{tag.name}}</a>
         <a class="btn" (click)="toggle()">
-            V
+            <span class="fa fa-chevron-down" aria-hidden="true"></span>
         </a>
 
         <ul class="dropdown-menu" [ngClass]="{ 'open': isOpen, 'closed': !isOpen }">
-            <li><a (click)="and()">Must contain</a></li>
-            <li><a (click)="or()">Can contain</a></li>
-            <li><a (click)="not()">Does not contain</a></li>
+            <li><a (click)="and()"><span class="fa fa-check-circle must-contain" aria-hidden="true"></span> Must contain</a></li>
+            <li><a (click)="or()"><span class="fa fa-question-circle can-contain" aria-hidden="true"></span> Can contain</a></li>
+            <li><a (click)="not()"><span class="fa fa-ban does-not-contain" aria-hidden="true"></span> Does not contain</a></li>
         </ul>
     </div>
     `
@@ -45,15 +45,18 @@ export class SearchTagComponent {
     private and(): void {
         this.tagClass = 'must-contain';
         this.changed.emit(new TagChange(this.tag, TagStatus.And));
+        this.toggle();
     }
 
     private or(): void {
         this.tagClass = 'can-contain';
         this.changed.emit(new TagChange(this.tag, TagStatus.Or));
+        this.toggle();
     }
 
     private not(): void {
         this.tagClass = 'does-not-contain';
         this.changed.emit(new TagChange(this.tag, TagStatus.Not));
+        this.toggle();
     }
 }
