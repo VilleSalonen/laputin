cafc5c78e7ae6cb63a723aff0eb546995a03612e
Ville Salonen <ville.salonen@iki.fi>
2018-07-06 16:05:32 +0300

    Cleaned up tags page

--- src/client/src/app/tags/tags.component.ts
+++ src/client/src/app/tags/tags.component.ts
@@ -6,57 +6,25 @@ import {Tag} from './../models/tag';
 import {LaputinService} from './../laputin.service';
 
 @Component({
-    template: `
-        <div class="filter-controls" style="margin-top: -20px; margin-bottom: 0px;">
-            <div class="extra-padded">
-                <div class="row">
-                    <div class="col-md-6">
-                        <form class="form-horizontal">
-                            <div class="form-group col-md-4">
-                                <label class="col-sm-2 control-label">Name</label>
-                                <div class="col-sm-10">
-                                    <div>
-                                        <app-search-box (update)="filter($event)"></app-search-box>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <table class="table table-striped">
-            <tbody>
-                <tr>
-                    <th>
-                        Showing {{filteredTags.length}} matching tags
-                    </th>
-                </tr>
-
-                <tr *ngFor="let tag of filteredTags">
-                    <td>
-                        <app-tag-row [tag]="tag"></app-tag-row>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    `,
-    providers: [LaputinService]
+    styleUrls: ['./tags.component.scss'],
+    templateUrl: './tags.component.html'
 })
 @Injectable()
 export class TagsComponent implements OnInit {
     public tags: Tag[] = [];
     public filteredTags: Tag[] = [];
+    public loading: boolean;
 
     constructor(@Inject(LaputinService) private _service: LaputinService) {
     }
 
     ngOnInit(): void {
+        this.loading = true;
         this._service.getTags()
             .then((tags) => {
                 this.tags = tags;
                 this.filteredTags = tags;
+                this.loading = false;
             });
     }
 
