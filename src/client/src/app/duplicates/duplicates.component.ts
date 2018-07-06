daf2f52ddb1519c190a6a06c2947eb9aa214b091
Ville Salonen <ville.salonen@iki.fi>
2018-07-06 16:10:41 +0300

    Cleaned up duplicates page

--- src/client/src/app/duplicates/duplicates.component.ts
+++ src/client/src/app/duplicates/duplicates.component.ts
@@ -5,49 +5,8 @@ import {LaputinService} from './../laputin.service';
 import {Duplicate} from './../models/duplicate';
 
 @Component({
-    template: `
-        <div class="filter-controls" style="margin-top: -20px; margin-bottom: 0px;">
-            <div class="extra-padded">
-                <div class="row">
-                    <div class="col-md-6">
-                        <form class="form-horizontal">
-                            <div class="form-group col-md-4">
-                                <label class="col-sm-2 control-label">Name</label>
-                                <div class="col-sm-10">
-                                    <div>
-                                        <app-search-box (update)="filter($event)"></app-search-box>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <table class="table table-striped">
-            <tbody>
-                <tr>
-                    <th>
-                        <span [hidden]="duplicates.length == 0">Showing {{filteredDuplicates.length}} matching duplicates</span>
-                        <span [hidden]="duplicates.length > 0">No duplicates found.</span>
-                    </th>
-                </tr>
-
-                <tr *ngFor="let duplicate of filteredDuplicates">
-                    <td>
-                        {{duplicate.hash}}
-                        <ul>
-                            <li *ngFor="let file of duplicate.files">
-                                {{file.path}}
-                            </li>
-                        </ul>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    `,
-    providers: [LaputinService]
+    styleUrls: ['./duplicates.component.scss'],
+    templateUrl: './duplicates.component.html'
 })
 @Injectable()
 export class DuplicatesComponent {
