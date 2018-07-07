311ef95c3875f7e8be57ae4708a356ce4a96611f
Ville Salonen <ville.salonen@iki.fi>
2018-07-07 13:24:13 +0300

    First working version of creating ffmpeg export commands

--- src/client/src/app/timecodes/timecodes.component.ts
+++ src/client/src/app/timecodes/timecodes.component.ts
@@ -7,6 +7,7 @@ import { Tag, Timecode } from './../models/tag';
 import { FileQuery } from './../models/filequery';
 import { LaputinService } from './../laputin.service';
 import { PlayerService } from '../player.service';
+import * as moment from 'moment';
 
 @Component({
     styleUrls: ['./timecodes.component.scss'],
@@ -19,6 +20,9 @@ export class TimecodesComponent implements OnInit {
     public timecodeItems: TimecodeItem[] = [];
     public viewPortItems: TimecodeItem[] = [];
     public loading = false;
+    public exporting = false;
+    public exportCommands: string[] = [];
+    public exportCommandsStr = '';
     private _query: FileQuery = new FileQuery();
 
     constructor(
@@ -34,6 +38,23 @@ export class TimecodesComponent implements OnInit {
         this.timecodeItems = [];
         this.loading = true;
         this._service.queryTimecodes(this._query).then((timecodes: Timecode[]) => {
+            this.exportCommands = [];
+            timecodes.forEach(t => {
+                const name = t.path.substring(t.path.lastIndexOf('/') + 1);
+
+                const tags = t.timecodeTags.map(ta => ta.tag.name).join(', ');
+
+                this.exportCommands.push('ffmpeg ' +
+                    '-ss ' + this.formatPreciseDuration(t.start) + ' ' +
+                    '-i "' + t.path.replace(/\//g, '\\') + '" ' +
+                    '-t ' + this.formatPreciseDuration(t.end - t.start) + ' ' +
+                    '-map v? -map a? -map s? -c:v hevc_nvenc -c:a copy -profile:v main -preset slow ' +
+                    '"TARGET_DIR\\' + name +
+                    ' (' + this.formatPreciseDuration(t.start).replace(/[\:]/g, '.') +
+                    '-' + this.formatPreciseDuration(t.start).replace(/[\:]/g, '.') + ') ['
+                    + tags + '].mp4"');
+            });
+
             const timecodesByFiles = _.groupBy(timecodes, (t: Timecode) => t.path);
 
             const timecodeItems: TimecodeItem[] = [];
@@ -48,9 +69,35 @@ export class TimecodesComponent implements OnInit {
         });
     }
 
+    private formatPreciseDuration(durationInSeconds: number): string {
+        const duration = moment.duration(durationInSeconds, 'seconds');
+
+        let result = '';
+
+        const hours = duration.hours();
+        const minutes = duration.minutes();
+        const seconds = duration.seconds();
+        const milliseconds = parseInt(duration.milliseconds().toFixed(0), 10);
+
+        result += ((hours >= 10) ? hours : '0' + hours);
+        result += ':';
+        result += (minutes >= 10) ? minutes : '0' + minutes;
+        result += ':';
+        result += (seconds >= 10) ? seconds : '0' + seconds;
+        result += '.';
+        result += (milliseconds > 100) ? milliseconds : (milliseconds >= 10) ? '0' + milliseconds : '00' + milliseconds;
+
+        return result;
+    }
+
     filterFiles(query: FileQuery): void {
         this._query = query;
         this.loadTimecodes();
+    }
+
+    public export(): void {
+        this.exporting = true;
+        this.exportCommandsStr = this.exportCommands.join('\r\n');
     }
 }
 
