7b86d41d02ae56bce1c84a9f4603efa996877207
Ville Salonen <ville.salonen@iki.fi>
2021-02-24 17:08:39 +0200

    file can be read from database

---
+++ Laputin/Model.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Laputin
+{
+    public class LaputinContext : DbContext
+    {
+        public DbSet<File> Files { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options
+                .UseSqlite(@"Data Source=C:\Github\.laputin.db");
+                //.LogTo(Console.WriteLine);
+    }
+
+    [Table("files")]
+    public class File
+    {
+        [Key]
+        [Column("hash")]
+        public string Hash { get; set; }
+
+        [Column("path")]
+        public string Path { get; set; }
+    }
+}
---
+++ Laputin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Laputin
+{
+    class Program
+    {
+        private static void Main()
+        {
+            using (var db = new LaputinContext())
+            {
+                // Note: This sample requires the database to be created before running.
+
+                // Create
+                // Console.WriteLine("Inserting a new blog");
+                // db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                // db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a file");
+                var file = db.Files
+                    .First();
+                Console.WriteLine(file.Path);
+
+                // Update
+                //Console.WriteLine("Updating the blog and adding a post");
+                //blog.Url = "https://devblogs.microsoft.com/dotnet";
+                //blog.Posts.Add(
+                //    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+                //db.SaveChanges();
+
+                // Delete
+                //Console.WriteLine("Delete the blog");
+                //db.Remove(blog);
+                //db.SaveChanges();
+            }
+        }
+    }
+}
