8cdfafe5bd202d7f50a46226e8da08ce03d8d4f8
Ville Salonen <ville.salonen@iki.fi>
2021-02-24 17:17:24 +0200

    library path is passed from command line

--- Laputin/Model.cs
+++ Laputin/Model.cs
@@ -7,11 +7,18 @@ namespace Laputin
 {
     public class LaputinContext : DbContext
     {
+        private string LibraryPath;
+
+        public LaputinContext(string libraryPath)
+        {
+            LibraryPath = libraryPath;
+        }
+
         public DbSet<File> Files { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options) =>
             options
-                .UseSqlite(@"Data Source=C:\Github\.laputin.db");
+                .UseSqlite($"Data Source={LibraryPath}");
                 //.LogTo(Console.WriteLine);
     }
 
--- Laputin/Program.cs
+++ Laputin/Program.cs
@@ -1,39 +1,62 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Laputin
 {
     class Program
     {
-        private static void Main()
+        public static async Task<int> Main(params string[] args)
         {
-            using (var db = new LaputinContext())
+            // Create a root command with some options
+            var rootCommand = new RootCommand
             {
-                // Note: This sample requires the database to be created before running.
+                new Option<string>(
+                    "--library-path",
+                    description: "Laputin path"),
+            };
 
-                // Create
-                // Console.WriteLine("Inserting a new blog");
-                // db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                // db.SaveChanges();
+            rootCommand.Description = "My sample app";
 
-                // Read
-                Console.WriteLine("Querying for a file");
-                var file = db.Files
-                    .First();
-                Console.WriteLine(file.Path);
+            // Note that the parameters of the handler method are matched according to the names of the options
+            rootCommand.Handler = CommandHandler.Create<string>((libraryPath) =>
+            {
+                using (var db = new LaputinContext(libraryPath))
+                {
+                    // Note: This sample requires the database to be created before running.
 
-                // Update
-                //Console.WriteLine("Updating the blog and adding a post");
-                //blog.Url = "https://devblogs.microsoft.com/dotnet";
-                //blog.Posts.Add(
-                //    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
-                //db.SaveChanges();
+                    // Create
+                    // Console.WriteLine("Inserting a new blog");
+                    // db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                    // db.SaveChanges();
 
-                // Delete
-                //Console.WriteLine("Delete the blog");
-                //db.Remove(blog);
-                //db.SaveChanges();
-            }
+                    // Read
+                    Console.WriteLine("Querying for a file");
+                    var file = db.Files
+                        .First();
+                    Console.WriteLine(file.Path);
+
+                    // Update
+                    //Console.WriteLine("Updating the blog and adding a post");
+                    //blog.Url = "https://devblogs.microsoft.com/dotnet";
+                    //blog.Posts.Add(
+                    //    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+                    //db.SaveChanges();
+
+                    // Delete
+                    //Console.WriteLine("Delete the blog");
+                    //db.Remove(blog);
+                    //db.SaveChanges();
+                }
+
+                Console.WriteLine($"The value for --library-path is: {libraryPath}");
+            });
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
         }
     }
 }
