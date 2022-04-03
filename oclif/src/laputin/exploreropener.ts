2e41c297542c01c9b141c4d5b0ee54c7266e2627
Ville Salonen <ville.salonen@iki.fi>
2022-04-03 13:32:34 +0300

    Fix opening of directory on macos

--- oclif/src/laputin/exploreropener.ts
+++ oclif/src/laputin/exploreropener.ts
@@ -2,13 +2,21 @@ import child_process = require('child_process');
 
 import { File } from './file';
 import winston = require('winston');
+import path = require('path');
 
 export class ExplorerOpener {
     private _binaryPath = 'C:\\WINDOWS\\explorer.exe';
 
     public open(files: File[]): void {
-        const filePath = files[0].path.replace(/\//g, '\\');
-        const command = `"${this._binaryPath}" /select,"${filePath}"`;
+        var command = '';
+        if (process.platform === 'win32') {
+            const filePath = files[0].path.replace(/\//g, '\\');
+            command = `"${this._binaryPath}" /select,"${filePath}"`;
+        } else {
+            const filePath = path.dirname(files[0].path);
+            command = `open "${filePath}"`;
+        }
+
         winston.verbose(`Executing ${command}...`);
         child_process.exec(command);
     }
