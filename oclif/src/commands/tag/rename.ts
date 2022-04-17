6bf9578497a9e0229caf5bda32893d1f2fc4cfde
Ville Salonen <ville.salonen@iki.fi>
2022-04-17 10:14:30 +0300

    Fix tag rename description

---
+++ oclif/src/commands/tag/rename.ts
@@ -0,0 +1,51 @@
+import { Command, Flags } from '@oclif/core';
+import { getLibraryPath } from '../../laputin/helpers';
+import { Library } from '../../laputin/library';
+import { initializeWinston } from '../../laputin/winston';
+import winston = require('winston');
+import { TagQuery } from '../../laputin/tagquery.model';
+
+export default class DeleteTagCommand extends Command {
+    static description = 'Renames a tag';
+
+    static examples = ['<%= config.bin %> <%= command.id %>'];
+
+    static flags = {
+        library: Flags.string({
+            char: 'l',
+            description: 'Laputin library path',
+            required: true,
+        }),
+        verbose: Flags.boolean({ char: 'v', default: false }),
+    };
+
+    static args = [
+        { name: 'oldTagName', required: true },
+        { name: 'newTagName', required: true },
+    ];
+
+    public async run(): Promise<void> {
+        const { args, flags } = await this.parse(DeleteTagCommand);
+
+        const oldTagName = <string>args.oldTagName;
+        const newTagName = <string>args.newTagName;
+
+        initializeWinston(flags.verbose);
+
+        const libraryPath = getLibraryPath(flags.library);
+        const library = new Library(libraryPath);
+
+        const allTags = await library.getTags(new TagQuery([], true));
+        const oldTagNameUpper = oldTagName.toLocaleUpperCase();
+        const oldTag = allTags.find(
+            (tag) => tag.name.toLocaleUpperCase() === oldTagNameUpper
+        );
+
+        if (oldTag) {
+            await library.renameTag(oldTag.id, newTagName);
+            winston.info(`Renamed tag ${oldTag.name} to ${newTagName}`);
+        } else {
+            winston.error(`Could not find tag with name ${oldTagName}`);
+        }
+    }
+}
