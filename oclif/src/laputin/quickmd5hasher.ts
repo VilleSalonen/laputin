68131fad7074bb7d1f05694172fb39e2e0c4583e
Ville Salonen <ville.salonen@iki.fi>
2022-08-25 17:06:50 +0300

    Fix QuickMD5Hasher to work with files with mostly different content but identical middle chunk
    This is a bit ugly but the idea is to find a chunk of data which contains non-identical bytes. In practice files with different content should contain non-identical middle chunk but in practice a couple of cases were observed where a middle chunk (and few chunks next to it) were full of uint 255 values.

--- oclif/src/laputin/quickmd5hasher.ts
+++ oclif/src/laputin/quickmd5hasher.ts
@@ -30,11 +30,46 @@ export class QuickMD5Hasher implements IHasher {
         fileHandle: fs.promises.FileHandle,
         stats: fs.Stats
     ): Promise<string> {
-        const input_size: number = stats.size;
-        const offset: number = Math.floor(input_size / 2.0 - CHUNK_SIZE / 2.0);
-        const buffer: Buffer = Buffer.alloc(CHUNK_SIZE);
+        const inputSize: number = stats.size;
 
-        await fileHandle.read(buffer, 0, buffer.length, offset);
+        const initialOffset: number = Math.floor(
+            inputSize / 2.0 - CHUNK_SIZE / 2.0
+        );
+        let offset = initialOffset;
+
+        let buffer: Buffer;
+        let allIdenticalBytes = true;
+        let firstPass = true;
+
+        // This is a bit ugly but the idea is to find a chunk of data which contains non-identical bytes.
+        // In practice files with different content should contain non-identical middle chunk but in practice
+        // a couple of cases were observed where a middle chunk (and few chunks next to it) were full of uint 255 values.
+        do {
+            if (offset + CHUNK_SIZE > inputSize) {
+                // Very rare case but let's at least throw an error instead of silently failing.
+                throw Error(
+                    'Could not find a chunk of non-identical bytes between the middle of the file and the end of the file!'
+                );
+            }
+
+            if (!firstPass) {
+                offset += CHUNK_SIZE;
+            }
+            firstPass = false;
+
+            buffer = Buffer.alloc(CHUNK_SIZE);
+            await fileHandle.read(buffer, 0, buffer.length, offset);
+
+            const firstByte = buffer[0];
+            for (var i = 1; i < buffer.length; i++) {
+                const currentByte = buffer[i];
+                if (currentByte !== firstByte) {
+                    allIdenticalBytes = false;
+                    break;
+                }
+            }
+        } while (allIdenticalBytes);
+
         fileHandle.close();
 
         return crypto.createHash('md5').update(buffer).digest('hex');
