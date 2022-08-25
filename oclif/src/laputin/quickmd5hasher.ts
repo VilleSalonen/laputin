7eadf65eb2938d514c7f35c0a42e3393b74c4790
Ville Salonen <ville.salonen@iki.fi>
2022-08-25 16:52:45 +0300

    Simplify QuickMD5Hasher with native fs.promises instead of promisify

--- oclif/src/laputin/quickmd5hasher.ts
+++ oclif/src/laputin/quickmd5hasher.ts
@@ -1,7 +1,5 @@
 import crypto = require('crypto');
 import * as fs from 'fs';
-import { promisify } from 'util';
-const fsOpen = promisify(fs.open);
 
 import { IHasher } from './ihasher';
 
@@ -10,12 +8,12 @@ const CHUNK_SIZE = 1024;
 export class QuickMD5Hasher implements IHasher {
     public async hash(path: string, stats: fs.Stats): Promise<string> {
         try {
-            const fd = await fsOpen(path, 'r');
-            if (typeof fd === 'undefined') {
+            const fileHandle = await fs.promises.open(path, 'r');
+            if (typeof fileHandle === 'undefined') {
                 return '';
             }
 
-            return await this.readHash(fd, stats);
+            return await this.readHash(fileHandle, stats);
         } catch (e) {
             console.log(e);
             if ((<any>e).name === 'TypeError') {
@@ -28,22 +26,17 @@ export class QuickMD5Hasher implements IHasher {
         }
     }
 
-    private readHash(fd: any, stats: fs.Stats): Promise<string> {
-        let done: Function;
-        const promise = new Promise<string>((resolve) => (done = resolve));
-
+    private async readHash(
+        fileHandle: fs.promises.FileHandle,
+        stats: fs.Stats
+    ): Promise<string> {
         const input_size: number = stats.size;
         const offset: number = Math.floor(input_size / 2.0 - CHUNK_SIZE / 2.0);
         const buffer: Buffer = Buffer.alloc(CHUNK_SIZE);
 
-        fs.read(fd, buffer, 0, buffer.length, offset, () => {
-            const hash = crypto.createHash('md5').update(buffer).digest('hex');
+        await fileHandle.read(buffer, 0, buffer.length, offset);
+        fileHandle.close();
 
-            fs.close(fd, () => {
-                done(hash);
-            });
-        });
-
-        return promise;
+        return crypto.createHash('md5').update(buffer).digest('hex');
     }
 }
