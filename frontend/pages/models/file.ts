e12d265e0526e8f293f70a870d628cbb8fc4408d
Ville Salonen <ville.salonen@iki.fi>
2022-09-18 14:05:34 +0300

    first few steps to loading files

---
+++ frontend/pages/models/file.ts
@@ -0,0 +1,41 @@
+import { Tag } from './tag';
+import { TagContainer } from './tagcontainer';
+
+export class File implements TagContainer {
+    public name: string;
+    public humanSize: string;
+    public escapedUrl: string;
+    public directory: string;
+    public nameSansSuffix: string;
+    public suffix: string;
+    public metadata: any;
+
+    constructor(
+        public hash: string,
+        public path: string,
+        public tags: Tag[],
+        public size: number,
+        public type: string,
+        metadata?: any
+    ) {
+        this.name = this.path.substring(this.path.lastIndexOf('/') + 1);
+        this.escapedUrl = this.path.replace('#', '%23').replace(/ /g, '%20');
+        this.directory = this.path.replace(this.name, '').replace(/\//g, '\\');
+        this.nameSansSuffix = this.name.substr(0, this.name.lastIndexOf('.'));
+        this.suffix = this.name.substr(this.name.lastIndexOf('.'));
+        this.humanSize = this.formatHumanSize();
+        this.metadata = metadata || {};
+    }
+
+    private formatHumanSize(): string {
+        if (this.size < 1000) {
+            return this.size + ' B';
+        } else if (this.size < 1000000) {
+            return (this.size / 1000.0).toFixed(1) + ' kB';
+        } else if (this.size < 1000000000) {
+            return (this.size / 1000000.0).toFixed(1) + ' MB';
+        } else {
+            return (this.size / 1000000000.0).toFixed(1) + ' GB';
+        }
+    }
+}
