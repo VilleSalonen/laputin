5ea9e672823ac7dbffe94a40fb2e84037fe21335
Ville Salonen <ville.salonen@iki.fi>
2022-10-16 14:15:52 +0300

    Remove unused variable

--- oclif/src/laputin/laputin.ts
+++ oclif/src/laputin/laputin.ts
@@ -217,7 +217,7 @@ export class Laputin {
         api.route(
             '/files/:hash/timecodes/:timecodeId/tags/:timecodeTagId'
         ).delete(async (req, res) => {
-            const result = await this.library.removeTagFromTimecode(
+            await this.library.removeTagFromTimecode(
                 req.params.hash,
                 req.params.timecodeId,
                 req.params.timecodeTagId
