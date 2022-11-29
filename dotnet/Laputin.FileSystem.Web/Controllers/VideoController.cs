37be269f0e0e1eda9609572ca6fd5f5692c26795
Ville Salonen <ville.salonen@iki.fi>
2022-11-29 18:31:42 +0200

    Remaining file properties included in model

--- dotnet/Laputin.FileSystem.Web/Controllers/VideoController.cs
+++ dotnet/Laputin.FileSystem.Web/Controllers/VideoController.cs
@@ -7,18 +7,40 @@ namespace Laputin.FileSystem.Web.Controllers;
 [Table("file")]
 public class LaputinFile {
     [Column("id")]
-    public int Id { get; set;}
+    public int Id { get; private set; }
 
     [Column("hash")]
-    public string? Hash {get; set;}
+    public string Hash { get; private set; }
 
     [Column("path")]
-    public string? Path {get; set;}
+    public string Path { get; private set; }
+
+    [Column("active")]
+    public int Active { get; private set; }
+
+    [Column("size")]
+    public int Size { get; private set; }
+
+    [Column("metadata")]
+    public string Metadata { get; private set; }
+
+    [Column("type")]
+    public string Type { get; private set; }
+
+    public LaputinFile(int id, string hash, string path, int active, int size, string metadata, string type) {
+        Id = id;
+        Hash = hash;
+        Path = path;
+        Active = active;
+        Size = size;
+        Metadata = metadata;
+        Type = type;
+    }
 }
 
 public class LaputinContext : DbContext
 {
-    public DbSet<LaputinFile>? LaputinFiles { get; set; }
+    public DbSet<LaputinFile> LaputinFiles => Set<LaputinFile>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseNpgsql("TODO");
